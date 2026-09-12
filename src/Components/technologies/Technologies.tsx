1b55b9bf0507cc1c9ddc4597b43dbdc10daa8aba
Muradul Islam <1053murad@gmail.com>
2026-09-13 02:08:37 +0600

    card modified as required

--- src/Components/technologies/Technologies.tsx
+++ src/Components/technologies/Technologies.tsx
@@ -1,17 +1,30 @@
 import React, { use } from 'react';
 import type { Itechnology } from '../../types/technology';
+import AvailableTechnologies from './AvailableTechnologies';
 
-interface TechnologiesProps{
+interface TechnologiesProps {
     technologiesPromise: Promise<Itechnology[]>
 }
 
-const Technologies = ({technologiesPromise}: TechnologiesProps ) => {
+const Technologies = ({ technologiesPromise }: TechnologiesProps) => {
     // console.log(technologiesPromise);
     const technologies = use(technologiesPromise);
     console.log(technologies);
     return (
-        <div>
-            
+        <div className='container mx-auto'>
+
+            <div className='flex justify-between gap-4 mb-2'>
+                <h2 className='font-bold text-xl'>Available technologies</h2>
+
+                <div>
+                    <button className='btn btn-success'>Available</button>
+                    <button className='btn '>Selected</button>
+                </div>
+            </div>
+
+
+            <AvailableTechnologies technologies={technologies}></AvailableTechnologies>
+
         </div>
     );
 };
